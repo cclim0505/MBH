0001a2a9dbfd02c484685e2754117cfdd3bf7d1d
cclim0505 <cclim.0505@gmail.com>
2020-12-08 01:35:09 +0800

    update runCage subprogram, specify how many atoms to move 	modified:   src/main_cage.f 	deleted:    00_check_MC_plot/MC_overview.dat 	deleted:    00_check_MC_plot/MC_overview.png

--- src/main_cage.f
+++ src/main_cage.f
@@ -1,14 +1,19 @@
         PROGRAM main
         USE coord_grad_ene          ,ONLY:atoms, material,coord
      &    ,allocate_coord_gradient ,printout_xyz
-        USE geometric_drive         ,ONLY:multi_cage_drive
+        USE geometric_drive         ,ONLY:multi_cage_move
         IMPLICIT NONE
         CHARACTER(LEN=30)       :: input_file
         INTEGER                 :: iter
         INTEGER                 :: f_atoms, f_coord
+        INTEGER                 :: num_moves
+        CHARACTER(len=20)       :: num_moves_text
         CHARACTER(len=1)        :: dummy
 
         CALL GET_COMMAND_ARGUMENT(1,input_file)
+        CALL GET_COMMAND_ARGUMENT(2,num_moves_text)
+
+        READ(num_moves_text,*) num_moves
 
         OPEN(NEWUNIT=f_atoms,FILE=TRIM(input_file), STATUS='old')
         READ(f_atoms,*) atoms
@@ -28,7 +33,8 @@
         END DO
         CLOSE(f_coord)
 
-        CALL multi_cage_drive
+
+        CALL multi_cage_move(num_moves,coord)
 
         CALL printout_xyz('moved_cage.xyz',coord)
 
